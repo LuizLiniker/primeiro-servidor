3bde9b35d146a3ca5f27c9ae432d45d0f90e79a8
LuizLiniker <linikerbraz32@gmail.com>
2026-05-14 22:02:59 -0300

    feat: criei 3 rotas para o meu servidor

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+//Estou pedindo emprestado o módulo http
+//Que já vem dentro do node.js
 const http = require("http")
 
-const servidor = http.createServer(function(req, res){
-    res.end("Servidor Funcionando")
+//defino minha porta - meu canal de escuta
+const PORTA = 3000
+
+//crio o servidor 
+//req = o pedido que chega
+//res = a resposta que eu mando de volta
+const servidor = http.createServer((req, res) => {
+
+    if(req.url == "/"){
+        res.end("Página inicial")
+    }
+    else if(req.url == "/usuarios"){
+        res.end("Lista de usuarios")
+    }
+    else{
+        res.end("Página não encontrada")
+    }
+    
 })
 
-servidor.listen(3000, function(){
-    console.log("Servidor rodando na porta 3000")
+//Mando o servidor começar a escutar
+
+servidor.listen(PORTA, () => {
+    console.log(`Servidor rodando na porta ${PORTA}`)
 })
